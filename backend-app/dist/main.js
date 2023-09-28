b25dd5b365e9e89d521b781c653b0b7aacb32ca4
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-09-28 20:22:46 +1300

    Added some new vercel commands

--- backend-app/dist/main.js
+++ backend-app/dist/main.js
@@ -2,7 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const dotenv = require("dotenv");
 async function bootstrap() {
+    dotenv.config();
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     await app.listen(8080);
 }
