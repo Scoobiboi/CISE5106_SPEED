503ef76ef9b52f3142326865da358095f84925ab
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-09-28 21:57:39 +1300

    Adding an API endPoint to the Backend to test

--- backend-app/src/app.controller.ts
+++ backend-app/src/app.controller.ts
@@ -9,4 +9,9 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('api/hello') // Add this endpoint
+  getHelloApi(): string {
+    return 'Hello from API!';
+  }
 }
