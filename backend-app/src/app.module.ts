1fec2f30eb4bc0864ec980bf747614d1b195dc75
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-10-01 14:36:29 +1300

    Added some stuff

--- backend-app/src/app.module.ts
+++ backend-app/src/app.module.ts
@@ -1,13 +1,35 @@
-import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CatsModule } from './cats/schema/cats.module';
 
-@Module({
-  imports: [
-    MongooseModule.forRoot(
-      `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/test`
-    ),
-    CatsModule,
-  ],
-})
-export class AppModule {}
+import { Injectable } from '@nestjs/common';
+import { MongoClient } from 'mongodb';
+
+@Injectable()
+export class MongoService {
+  private client: MongoClient;
+
+  async connectClient(): Promise<string> {
+    if (!this.client) {
+      try {
+        const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=AtlasApp`;
+        this.client = new MongoClient(uri);
+        await this.client.connect();
+        return 'connected';
+      } catch (error) {
+        console.error('MongoDB connection error:', error);
+        return 'disconnected';
+      }
+    } else {
+      return 'already connected';
+    }
+  }
+
+  async closeConnection() {
+    if (this.client) {
+      await this.client.close();
+      this.client = undefined;
+    }
+  }
+
+  getConnectionInfo() {
+    return this.connectClient();
+  }
+}
