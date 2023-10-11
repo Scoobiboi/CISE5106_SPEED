7a8d5b19aba22400c982e1bace58fc0254928fd8
Timothy Young <bht4069@autuni.ac.nz>
2023-10-11 19:34:23 +1300

    created localhost:3000/articles page with all articles retrieved and sorted by date.

---
+++ src/app/articles/page.jsx
@@ -0,0 +1,10 @@
+import { ArticlesComponent } from "../components/RetrieveArticles";
+import "./articles.css";
+
+export default function Home() {
+  return (
+    <div className="articles-page-container">
+      <ArticlesComponent />
+    </div>
+  );
+}
