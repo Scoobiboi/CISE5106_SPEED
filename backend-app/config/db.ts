39e52602ca4ebf2fade927ff7929bfab32be5301
Scoobiboi <lukeanthonyteuamcleod@gmail.com>
2023-10-17 12:28:56 +1300

    Added Try Catch methods as data was not being presented under the appropriate api calls from Mongo. Had a MongoDB error

--- backend-app/config/db.ts
+++ backend-app/config/db.ts
@@ -1,25 +1,27 @@
+//The previous code is commented out below... 
+
 import { MongoClient, ObjectId } from 'mongodb';
 
 let client;
 
 export async function connectClient() {
-  //remove as string if this doesn't work. 
-  const uri = `mongodb+srv://${process.env.DB_USER as string}:${process.env.DB_PASS as string}@${process.env.DB_HOST as string}/?retryWrites=true&w=majority&appName=AtlasApp`;
+  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority&appName=AtlasApp`;
   client = new MongoClient(uri);
   console.log('MongoDB URI:', uri);
   console.log('DB_USER:', process.env.DB_USER);
   console.log('DB_PASS:', process.env.DB_PASS);
   console.log('DB_HOST:', process.env.DB_HOST);
+  
   try {
     await client.connect();
-    return Promise.resolve('connected');
+    return 'connected';
   } catch (e) {
     console.error(e);
-    return Promise.resolve('disconnected');
+    return 'disconnected';
   }
 }
 
-export function getConnectionInfo(): Promise<string> {
+export function getConnectionInfo() {
   return connectClient();
 }
 
@@ -40,7 +42,6 @@ export async function getArticles(sortBy = '_id') {
   }
 }
 
-//Update status
 export async function updateArticleStatus(id, status) {
   try {
     const result = await client
@@ -54,7 +55,6 @@ export async function updateArticleStatus(id, status) {
   }
 }
 
-// Insert New Article
 export async function addArticle(article) {
   try {
     const result = await client
@@ -67,29 +67,127 @@ export async function addArticle(article) {
     return null;
   }
 }
+
+export async function rateArticle(id, newRating) {
+  try {
+    const article = await client
+      .db('CISE_SPEED_DATABASE')
+      .collection('Articles')
+      .findOne({ _id: new ObjectId(id) });
+    
+    const updatedRating = Math.round((article.Rating * article.no_Ratings + newRating) / (article.no_Ratings + 1));
+    const updatedNoRatings = article.no_Ratings + 1;
+
+    const result = await client
+      .db('CISE_SPEED_DATABASE')
+      .collection('Articles')
+      .updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { Rating: updatedRating, no_Ratings: updatedNoRatings } }
+      );
+    return result;
+  } catch (e) {
+    console.error(e);
+    return null;
+  }
+}
+
+
+
+
+
+// import { MongoClient, ObjectId } from 'mongodb';
+
+// let client;
+
+// export async function connectClient() {
+//   //remove as string if this doesn't work. 
+//   const uri = `mongodb+srv://${process.env.DB_USER as string}:${process.env.DB_PASS as string}@${process.env.DB_HOST as string}/?retryWrites=true&w=majority&appName=AtlasApp`;
+//   client = new MongoClient(uri);
+//   console.log('MongoDB URI:', uri);
+//   console.log('DB_USER:', process.env.DB_USER);
+//   console.log('DB_PASS:', process.env.DB_PASS);
+//   console.log('DB_HOST:', process.env.DB_HOST);
+//   try {
+//     await client.connect();
+//     return Promise.resolve('connected');
+//   } catch (e) {
+//     console.error(e);
+//     return Promise.resolve('disconnected');
+//   }
+// }
+
+// export function getConnectionInfo(): Promise<string> {
+//   return connectClient();
+// }
+
+// export async function getArticles(sortBy = '_id') {
+//   try {
+//     const sortParams = {};
+//     sortParams[sortBy] = 1; // Sort in ascending order
+//     const articles = await client
+//       .db('CISE_SPEED_DATABASE')
+//       .collection('Articles')
+//       .find()
+//       .sort(sortParams)
+//       .toArray();
+//     return articles;
+//   } catch (e) {
+//     console.error(e);
+//     return [];
+//   }
+// }
+
+// //Update status
+// export async function updateArticleStatus(id, status) {
+//   try {
+//     const result = await client
+//       .db('CISE_SPEED_DATABASE')
+//       .collection('Articles')
+//       .updateOne({ _id: new ObjectId(id) }, { $set: { Moderation_status: status } }); // Convert id to ObjectId
+//     return result;
+//   } catch (e) {
+//     console.error(e);
+//     return null;
+//   }
+// }
+
+// // Insert New Article
+// export async function addArticle(article) {
+//   try {
+//     const result = await client
+//       .db('CISE_SPEED_DATABASE')
+//       .collection('Articles')
+//       .insertOne(article);
+//     return result;
+//   } catch (e) {
+//     console.error(e);
+//     return null;
+//   }
+// }
   
 
-  //New Rating
-  export async function rateArticle(id, newRating) {
-    try {
-      const article = await client
-        .db('CISE_SPEED_DATABASE')
-        .collection('Articles')
-        .findOne({ _id: new ObjectId(id) });
+//   //New Rating
+//   export async function rateArticle(id, newRating) {
+//     try {
+//       const article = await client
+//         .db('CISE_SPEED_DATABASE')
+//         .collection('Articles')
+//         .findOne({ _id: new ObjectId(id) });
       
-      const updatedRating = Math.round((article.Rating * article.no_Ratings + newRating) / (article.no_Ratings + 1));
-      const updatedNoRatings = article.no_Ratings + 1;
+//       const updatedRating = Math.round((article.Rating * article.no_Ratings + newRating) / (article.no_Ratings + 1));
+//       const updatedNoRatings = article.no_Ratings + 1;
   
-      const result = await client
-        .db('CISE_SPEED_DATABASE')
-        .collection('Articles')
-        .updateOne(
-          { _id: new ObjectId(id) },
-          { $set: { Rating: updatedRating, no_Ratings: updatedNoRatings } }
-        );
-      return result;
-    } catch (e) {
-      console.error(e);
-      return null;
-    }
-  }
+//       const result = await client
+//         .db('CISE_SPEED_DATABASE')
+//         .collection('Articles')
+//         .updateOne(
+//           { _id: new ObjectId(id) },
+//           { $set: { Rating: updatedRating, no_Ratings: updatedNoRatings } }
+//         );
+//       return result;
+//     } catch (e) {
+//       console.error(e);
+//       return null;
+//     }
+//   }
--- backend-app/src/app.controller.ts
+++ backend-app/src/app.controller.ts
@@ -1,84 +1,172 @@
+//Previous code is commented out. 
+
 import { Controller, Get, Put, Post, Body, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { getArticles, updateArticleStatus, addArticle, rateArticle } from '../config/db';
+import {
+  getArticles,
+  updateArticleStatus,
+  addArticle,
+  rateArticle,
+} from '../config/db';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('/')
-  getHello(): Promise<string> {
-    return this.appService.getHello();
+  async getHello(): Promise<string> {
+    try {
+      const result = await this.appService.getHello();
+      return result;
+    } catch (error) {
+      // Handle the error and send an appropriate response
+      throw new Error('Failed to get hello');
+    }
   }
 
   @Get('/articles')
   async getArticles() {
-    const articles = await getArticles();
-    return articles;
+    try {
+      const articles = await getArticles();
+      return articles;
+    } catch (error) {
+      // Handle the error and send an appropriate response
+      throw new Error('Failed to get articles');
+    }
   }
 
-  @Get('/title/articles')
-  async getArticlesByTitle() {
-    const articles = await getArticles('title');
-    return articles;
-  }
+  // Other controller methods...
 
-  @Get('/year/articles')
-  async getArticlesByYear() {
-    const articles = await getArticles('Publication_year');
-    return articles;
-  }
-
-  @Get('/status/articles')
-  async getArticlesByStatus() {
-    const articles = await getArticles('Moderation_status');
-    return articles;
-  }
-
-  @Get('/authors/articles')
-  async getArticlesByAuthor() {
-    const articles = await getArticles('Authors');
-    return articles;
-  }
-
-  @Get('/rating/articles')
-  async getArticlesByRating() {
-    const articles = await getArticles('Rating');
-    return articles;
-  }
-
-  // Body in put request must look like this: { "status": false }
-  @Put('/articles/:id/status') // New PUT route
+  @Put('/articles/:id/status')
   async updateArticleStatus(@Param('id') id: string, @Body('status') status: boolean) {
-    const result = await updateArticleStatus(id, status);
-    return result;
+    try {
+      const result = await updateArticleStatus(id, status);
+      return result;
+    } catch (error) {
+      // Handle the error and send an appropriate response
+      throw new Error('Failed to update article status');
+    }
   }
 
-  // {
-  //  "title": "Arin POST test 2",
-  //  "Authors": "Arin Bindra",
-  //  "Journal_Name": "Arin Posted Articles",
-  //  "Publication_year": 2022 
-  //  }
-  @Post('/articles') // New POST route
+  @Post('/articles')
   async addArticle(@Body() body) {
-    const article = {
-      title: body.title,
-      Authors: body.Authors,
-      Journal_Name: body.Journal_Name,
-      Publication_year: body.Publication_year,
-      Moderation_status: false,
-      Rating: 0,
-      no_Ratings: 0
-    };
-    const result = await addArticle(article);
-    return result;
+    try {
+      const article = {
+        title: body.title,
+        Authors: body.Authors,
+        Journal_Name: body.Journal_Name,
+        Publication_year: body.Publication_year,
+        Moderation_status: false,
+        Rating: 0,
+        no_Ratings: 0,
+      };
+      const result = await addArticle(article);
+      return result;
+    } catch (error) {
+      // Handle the error and send an appropriate response
+      throw new Error('Failed to add an article');
+    }
   }
 
-  //{ "rating": 5 }
-  @Put('/articles/:id/rate') // New PUT route for rating
+  @Put('/articles/:id/rate')
   async rateArticle(@Param('id') id: string, @Body('rating') rating: number) {
-    const result = await rateArticle(id, rating);
-    return result;
+    try {
+      const result = await rateArticle(id, rating);
+      return result;
+    } catch (error) {
+      // Handle the error and send an appropriate response
+      throw new Error('Failed to rate the article');
+    }
   }
 }
+
+
+
+
+
+
+
+//import { Controller, Get, Put, Post, Body, Param } from '@nestjs/common';
+// import { AppService } from './app.service';
+// import { getArticles, updateArticleStatus, addArticle, rateArticle } from '../config/db';
+
+// @Controller()
+// export class AppController {
+//   constructor(private readonly appService: AppService) {}
+
+//   @Get('/')
+//   getHello(): Promise<string> {
+//     return this.appService.getHello();
+//   }
+
+//   @Get('/articles')
+//   async getArticles() {
+//     const articles = await getArticles();
+//     return articles;
+//   }
+
+//   @Get('/title/articles')
+//   async getArticlesByTitle() {
+//     const articles = await getArticles('title');
+//     return articles;
+//   }
+
+//   @Get('/year/articles')
+//   async getArticlesByYear() {
+//     const articles = await getArticles('Publication_year');
+//     return articles;
+//   }
+
+//   @Get('/status/articles')
+//   async getArticlesByStatus() {
+//     const articles = await getArticles('Moderation_status');
+//     return articles;
+//   }
+
+//   @Get('/authors/articles')
+//   async getArticlesByAuthor() {
+//     const articles = await getArticles('Authors');
+//     return articles;
+//   }
+
+//   @Get('/rating/articles')
+//   async getArticlesByRating() {
+//     const articles = await getArticles('Rating');
+//     return articles;
+//   }
+
+//   // Body in put request must look like this: { "status": false }
+//   @Put('/articles/:id/status') // New PUT route
+//   async updateArticleStatus(@Param('id') id: string, @Body('status') status: boolean) {
+//     const result = await updateArticleStatus(id, status);
+//     return result;
+//   }
+
+//   // {
+//   //  "title": "Arin POST test 2",
+//   //  "Authors": "Arin Bindra",
+//   //  "Journal_Name": "Arin Posted Articles",
+//   //  "Publication_year": 2022 
+//   //  }
+//   @Post('/articles') // New POST route
+//   async addArticle(@Body() body) {
+//     const article = {
+//       title: body.title,
+//       Authors: body.Authors,
+//       Journal_Name: body.Journal_Name,
+//       Publication_year: body.Publication_year,
+//       Moderation_status: false,
+//       Rating: 0,
+//       no_Ratings: 0
+//     };
+//     const result = await addArticle(article);
+//     return result;
+//   }
+
+//   //{ "rating": 5 }
+//   @Put('/articles/:id/rate') // New PUT route for rating
+//   async rateArticle(@Param('id') id: string, @Body('rating') rating: number) {
+//     const result = await rateArticle(id, rating);
+//     return result;
+//   }
+// }
